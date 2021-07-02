bc0ca617706fa64d5294f98b5c706526e451fa6d
Elliot Powell <johnp8727@gmail.com>
2021-07-02 23:42:04 +0100

    further update

--- app/boards/arm/ep75/board.cmake
+++ app/boards/arm/ep75/board.cmake
@@ -1,4 +1,7 @@
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+# Copyright (c) 2021 Elliot Powell
+# SPDX-License-Identifier: MIT
 
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
